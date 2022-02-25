d7f66917cae4545ca0c68ccbe8ecdacd40419cfb
Matheus Amancio Miranda <matheus.amancio98@gmail.com>
2022-02-25 13:39:24 -0300

    🐛 Corrige exibição das cargas nodais nos resultados

--- Frame/Frame_2D.py
+++ Frame/Frame_2D.py
@@ -804,7 +804,7 @@ class Results:
             if node.has_applied_node_load:
                 Fx, Fy, Mz = node.applied_node_load.vector
 
-                self.output.insert(row_to_write, f"{node.id + 1} | {Fx} | {Fy} | {Mz}")
+                self.output.insert(row_to_write, f"{node.id + 1} | {Fx:+6.4} | {Fy:+6.4} | {Mz:+6.4}")
                 row_to_write += 1
 
     def write_applied_member_loads(self):
